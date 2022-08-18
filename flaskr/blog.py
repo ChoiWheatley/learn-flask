abbd0796e7479789c29a13acd6f5c3747dc98fb0
ChoiWheatley <chltmdgus604@naver.com>
2022-08-18 22:08:03 +0900

    Update, Delete Complete!

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -45,3 +45,66 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    """포스트 id에 대응하는 포스트를 가져와서 author_id와
+    사용자 id가 일치하는지 비교한다. update, delete 함수가 사용한다."""
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=['GET', 'POST'])
+@login_required
+def update(id: int):
+    """`url_for('blog.update', id=post['id'])`함수와 연결된다. 
+    이때 전달된 id가 update 앞에 붙는다는 의미에서 저 `<int:id>`
+    구문이 들어간 것임."""
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id,)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=['POST'])
+@login_required
+def delete(id):
+    """주어진 id로 포스트를 삭제한다."""
+    get_post(id)
+    db = get_db()
+    db.execute(
+        'DELETE FROM post WHERE id = ?', (id,)
+    )
+    db.commit()
+    return redirect(url_for('blog.index'))
